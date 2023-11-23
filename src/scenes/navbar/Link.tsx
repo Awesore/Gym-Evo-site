e2a8d851bf07c8babe34c810aacd06d2504332e9
Vadim <vadim.slisarenko.03@gmail.com>
2023-11-23 16:38:29 +0200

    last commit

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,26 @@
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import ourClasses from '@/scenes/ourClasses';
+
+type Props = {
+    page: string;
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;
+};
+
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+    const lowerCasePage = page.toLowerCase().replace(/ /g, '');
+
+    return (
+        <AnchorLink
+            href={`#${lowerCasePage}`}
+            onClick={() => setSelectedPage(lowerCasePage)}
+            className={`${
+                selectedPage === lowerCasePage ? 'text-primary-500' : ''
+            } transition duration-500 hover:text-primary-300`}
+        >
+            {page}
+        </AnchorLink>
+    );
+};
+
+export default Link;
